35d6ffdf8b0a49d5164207b4c8865861b83ec1bf
UniConnect-UCSC <nethunanaykkara315@mail.com>
2026-06-12 09:11:42 +0530

    feat: compare two strings and check equality

---
+++ src/Q5.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val str1 = "Kotlin"
+    val str2 = "kotlin"
+
+    if (str1 == str2) {
+        println("Strings are equal")
+    } else {
+        println("Strings are not equal")
+    }
+}
