31d50930352440ce8e54ce5025fc0318417f0580
Nethmi Nimasha <nethunanayakkara315@gmail.com>
2026-06-12 09:30:27 +0530

    feat: add example showing immutable and mutable variables

---
+++ src/Q7.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    val noteTitle = "Meeting Notes"
+
+
+    var noteContent = "Discuss project requirements."
+
+    println("Title: $noteTitle")
+    println("Content: $noteContent")
+
+
+    noteContent = "Discuss project requirements and timeline."
+
+    println("Updated Content: $noteContent")
+
+}
