7ad3c89de806015016a6dc30df179f0391f206e3
UniConnect-UCSC <nethunanaykkara315@mail.com>
2026-06-14 15:46:12 +0530

    some changes

--- src/Q09.kt
+++ src/Q09.kt
@@ -15,12 +15,10 @@ class Passenger(
 
 fun main() {
 
-    // a. Create multiple passenger objects
     val passenger1 = Passenger("Nethmi Nimasha", "N1234567", "Dubai")
     val passenger2 = Passenger("Kavindu Perera", "N7654321", "Singapore")
     val passenger3 = Passenger("Tharushi Silva", "N1122334", "London")
 
-    // b. Display ticket information
     passenger1.displayTicketInfo()
     passenger2.displayTicketInfo()
     passenger3.displayTicketInfo()
